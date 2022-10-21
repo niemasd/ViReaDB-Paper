58f01dc5d6dd41486a0d1f92c7b6398a0d949bea
Niema Moshiri <niemamoshiri@gmail.com>
2022-10-20 17:29:20 -0700

    BAM --> CRAM

--- scripts/cram_to_vireadb.py
+++ scripts/cram_to_vireadb.py
@@ -1,6 +1,6 @@
 #! /usr/bin/env python3
 '''
-Create a ViReaDB database from a given BAM
+Create a ViReaDB database from a given CRAM
 '''
 from os.path import isfile
 from resource import getrusage, RUSAGE_SELF
@@ -12,8 +12,8 @@ import argparse
 # main content
 if __name__ == "__main__":
     if len(argv) != 4:
-        print("USAGE: %s <input_bam> <ref_fas> <output_db>" % argv[0]); exit(1)
-    assert isfile(argv[1]), "Input BAM file not found: %s" % argv[1]
+        print("USAGE: %s <input_cram> <ref_fas> <output_db>" % argv[0]); exit(1)
+    assert isfile(argv[1]), "Input CRAM file not found: %s" % argv[1]
     assert isfile(argv[2]), "Reference FASTA file not found: %s" % argv[2]
     assert not isfile(argv[3]), "Output DB file exists: %s" % argv[3]
     t1 = time()
